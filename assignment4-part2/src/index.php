f0a7009e4b387d348839bf8888ac70e7cfe62bfd
Tim Robinson <robinsti@engr.orst.edu>
2015-05-06 09:16:52 -0700

    Added input validation, filter by category

--- assignment4-part2/src/index.php
+++ assignment4-part2/src/index.php
@@ -6,7 +6,7 @@ if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 
 } else {
-	echo "Connection worked!<br>";
+	echo "Video library connected!<br><br>";
 }
 
 echo '<!DOCTYPE html>
@@ -46,13 +46,11 @@ if(!$mysqli->query("CREATE TABLE IF NOT EXISTS videos(
 
 //Insert elements in to table
 
+
 // Prepare statement
 if(!($stmt = $mysqli->prepare("INSERT INTO videos(id, name, category, length, rented) VALUES (?, ?, ?, ?, ?)"))) {
 	echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
-
-
-
 
 // Bind and execute prepared statement
 
@@ -62,23 +60,37 @@ $rented = 1;
 echo '<form action="index.php" method="get" name="add_form">
 	Title: <input type="text" name="name_field"><br>
 		Category: <input type="text" name="cat_field"><br>
-		Length: <input type="text" name="length_field"><br>
+		Length: <input type="number" name="length_field" min="0" max="10000"><br>
 		<input type="submit" name="sub_button" value="Add Video"><br>
 	</form>';
 
-if(isset($_GET["name_field"])) {
-	$name = $_GET["name_field"];
-	$cat = $_GET["cat_field"];
-	$length = $_GET["length_field"];
+do {
 
-	if(!$stmt->bind_param("issii", $id, $name, $cat, $length, $rented)) {
-		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
+	if(isset($_GET["name_field"])) {
+		$name = $_GET["name_field"];
+		$cat = $_GET["cat_field"];
+		$length = $_GET["length_field"];
 
-	if(!$stmt->execute()) {
-		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		if(($name == '') || ($name == ' ')) {
+			echo "You must provide a name!<br>";
+			break;
+		} else if(isset($_GET["length_field"])) {
+			if(is_int($length)) {
+				echo "Length must be a positive integer!<br>";
+				break;
+			}
+		} 
+		
+
+		if(!$stmt->bind_param("issii", $id, $name, $cat, $length, $rented)) {
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+
+		if(!$stmt->execute()) {
+			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
 	}
-}
+} while(0);
 
 if(isset($_GET["rentalChange"])) {
 	$tmp = $_GET["rentalChange"];
@@ -95,6 +107,47 @@ if(isset($_GET["deleteEntry"])) {
 }
 
 // Get elements from the table
+
+
+
+
+echo 'Show: 
+	<form action="index.php" method="get">
+		<select name="catSelect">
+			<option value="all_movies">All Movies</option>';
+
+$tmpArr = array();
+
+
+if(!($stmt = $mysqli->prepare("SELECT category FROM videos"))) {
+	echo "Prepare failed (" . $mysqli->errno . ") " . $mysqli->error;
+}
+if(!$stmt->execute()) {
+	echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+$tmpCat = NULL;
+$count = 0;
+if(!$stmt->bind_result($tmpCat)) {
+	echo "Binding output parameter failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+while($stmt->fetch()) {
+	if((!in_array($tmpCat, $tmpArr)) && ($tmpCat != '')) {
+		echo '<option>' . $tmpCat . '</option>';
+		$tmpArr[$count] = $tmpCat;
+		$count = $count + 1;
+	}
+}
+
+echo	'</select>
+		<input type="submit" value="Filter">
+	</form>';
+
+
+
+
+
 
 if(!($stmt = $mysqli->prepare("SELECT id, name, category, length, rented FROM videos"))) {
 	echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
@@ -114,7 +167,7 @@ if(!$stmt->bind_result($out_id, $out_name, $out_cat, $out_length, $out_rented)) 
 	echo "Binding output paramters failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 
-	echo '<table>
+echo '<table>
 	<tr>
 		<td>ID
 		<td>Name
@@ -125,26 +178,74 @@ if(!$stmt->bind_result($out_id, $out_name, $out_cat, $out_length, $out_rented)) 
 
 
 while($stmt->fetch()) {
-	echo '<tr>';
-	echo '<td>' . $out_id;
-	echo '<td>' . $out_name;
-	echo '<td>' . $out_cat;
-	echo '<td>' . $out_length;
-	if($out_rented == 1) {
-		echo '<td>Available';
+	if(isset($_GET["catSelect"])) {
+		$tmpCat = $_GET["catSelect"];
+		if($tmpCat == $out_cat) {
+			echo '<tr>';
+			echo '<td>' . $out_id;
+			echo '<td>' . $out_name;
+			echo '<td>' . $out_cat;
+			echo '<td>' . $out_length;
+			if($out_rented == 1) {
+				echo '<td>Available';
+			} else {
+				echo '<td>Checked Out';
+			}
+			echo '<td>
+				<form action="index.php" method="get">
+					<input type="hidden" name="rentalChange" value="' . $out_id . '" />
+					<input type="submit" value="Check In/Out">
+				</form>';
+			echo '<td>
+				<form action="index.php" method="get">
+					<input type="hidden" name="deleteEntry" value="' . $out_id . '" />
+					<input type="submit" value="Delete">
+				</form>';
+		} else if($tmpCat == 'all_movies'){
+			echo '<tr>';
+			echo '<td>' . $out_id;
+			echo '<td>' . $out_name;
+			echo '<td>' . $out_cat;
+			echo '<td>' . $out_length;
+			if($out_rented == 1) {
+				echo '<td>Available';
+			} else {
+				echo '<td>Checked Out';
+			}
+			echo '<td>
+				<form action="index.php" method="get">
+					<input type="hidden" name="rentalChange" value="' . $out_id . '" />
+					<input type="submit" value="Check In/Out">
+				</form>';
+			echo '<td>
+				<form action="index.php" method="get">
+					<input type="hidden" name="deleteEntry" value="' . $out_id . '" />
+					<input type="submit" value="Delete">
+				</form>';
+			}
 	} else {
-		echo '<td>Checked Out';
+		echo '<tr>';
+		echo '<td>' . $out_id;
+		echo '<td>' . $out_name;
+		echo '<td>' . $out_cat;
+		echo '<td>' . $out_length;
+		if($out_rented == 1) {
+			echo '<td>Available';
+		} else {
+			echo '<td>Checked Out';
+		}
+		echo '<td>
+			<form action="index.php" method="get">
+				<input type="hidden" name="rentalChange" value="' . $out_id . '" />
+				<input type="submit" value="Check In/Out">
+			</form>';
+		echo '<td>
+			<form action="index.php" method="get">
+				<input type="hidden" name="deleteEntry" value="' . $out_id . '" />
+				<input type="submit" value="Delete">
+			</form>';
+
 	}
-	echo '<td>
-		<form action="index.php" method="get">
-			<input type="hidden" name="rentalChange" value="' . $out_id . '" />
-			<input type="submit" value="Check In/Out">
-		</form>';
-	echo '<td>
-		<form action="index.php" method="get">
-			<input type="hidden" name="deleteEntry" value="' . $out_id . '" />
-			<input type="submit" value="Delete">
-		</form>';
 }
 echo '</table><br>';
 
